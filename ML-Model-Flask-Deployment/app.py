9dc73d960c7cfd2be999c6569b5306c06ec308d8
arnab das <arnab0989@gmail.com>
2020-12-22 16:28:25 +0530

    First Commit in Proimary Branch

--- ML-Model-Flask-Deployment/app.py
+++ ML-Model-Flask-Deployment/app.py
@@ -5,15 +5,18 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    '''
+    """
     For rendering results on HTML GUI
-    '''
+    """
+    print("Predict is called")
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
@@ -22,16 +25,25 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
+
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    '''
-    For direct API calls trought request
-    '''
+    """
+    For direct API calls trough request
+    Json Sample
+    {"experience":2, "test_score":9, "interview_score":6}
+    """
+
+    print("Predict API is called")
     data = request.get_json(force=True)
+    print(data)
     prediction = model.predict([np.array(list(data.values()))])
 
     output = prediction[0]
     return jsonify(output)
 
+
 if __name__ == "__main__":
+    print("Before app run")
     app.run(debug=True)
+    print("main is called")
